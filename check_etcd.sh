467371b7ea00dd99f629c2b2f582d296591afb62
Chinmay Bhat <57489769+Chinmaybhat388@users.noreply.github.com>
2024-10-15 15:32:42 +0530

    Update check_etcd.sh

--- check_etcd.sh
+++ check_etcd.sh
@@ -7,12 +7,12 @@ clusters=("cluster1" "cluster2")
 
 for cluster in "${clusters[@]}"; do
 echo $cluster
-# Get the list of IP addresses from your command
+# Get the list of IP addresses from gcloud command
 gcloud compute instances list --format="table(metadata.items.consul_srv,name,networkInterfaces.networkIP,machineType,labels.db_technical_contact,labels.db_environment,labels.db_app_tier)[box,no-heading]" --filter="metadata.consul_srv=$cluster"
 
 nodes=$(gcloud compute instances list --format="get(networkInterfaces[0].networkIP)" --filter="$cluster")
 
-# Loop over each IP address in the nodes variable
+# Loop over IP addresses in the nodes variable
 for node in $nodes; do
     echo $node
     ssh -o StrictHostKeyChecking=no chinmay@$node "sudo cat /data1/pgdata/config/patroni.yml|grep -i -A 5 etcd| grep -i hosts"
