121f46b2fab63e4e3593609adc0debe0ce3db132
Chinmay Bhat <57489769+Chinmaybhat388@users.noreply.github.com>
2025-06-11 15:40:14 +0530

    Add files via upload

---
+++ get_cluster_resources.sh
@@ -0,0 +1,66 @@
+hosts=(lxpgsmsrvprdw02 lxpgsmsrvprdw01 lxpgsscrpprdw01 lxpgsscrpprdw02 lxpgsramsprdw02 lxpgsramsprdw01 lxpgsoaplprdw02 lxpgsoaplprdw01 lxpgspomaprdw03 lxpgspomaprdw02 lxpgspomaprdw01 lxpgstemsprdw02 lxpgstemsprdw01 lxpgsisvmprdw02 lxpgsisvmprdw01 lxpgscdpprdw002 lxpgscdpprdw001 lxhdpairprdw005 lxhdpairprdw004 lxpgsremmprdw01 lxpgssonrprdw02 lxpgssonrprdw01 lxpgsstasprdw02 lxpgsstasprdw01 lxpgsconfprdw01 lxpgsconfprdw02 lxpgsjiraprdw04 lxpgsjiraprdw03 lxpgsimptprdw01 lxpgsrcmsprdw02 lxpgsrcmsprdw01 lxpgsedptprdw03 lxpgsedptprdw02 lxpgsedptprdw01 lxpgsedptprdw04 lxpgsedpmprdw02 lxpgsedpmprdw01 lxpgsppssprdw02 lxpgseinvprdw02 lxpgseinvprdw04 lxpgseinvprdw03 lxpgseinvprdw08 lxpgseinvprdw07 lxpgseinvprdw01 lxpgsjfrgprdw01 lxpgsjfrgprdw02 lxpgsartiprdw02 lxpgsjfrgprdw03 lxpgsartiprdw01 lxpgsdaitprdw03 lxpgsdaitprdw02 lxpgspresprdw06 lxpgspresprdw05 lxpgspresprdw04 lxpgspresprdw03 lxpgspresprdw02 lxpgspresprdw01 lxpgslcfsprdw02 lxpgslcfsprdw01 lxpgsvrtxprdw04 lxpgsvrtxprdw03 lxpgsvrtxprdw02 lxpgsvrtxprdw01 lxpgsvrtfprdw17 lxpgsvrtfprdw16 lxpgsvrtfprdw14 lxpgsvrtfprdw13 lxpgsvrtfprdw12 lxpgsvrtfprdw11 lxpgsvrtfprdw10 lxpgsvrtfprdw09 lxpgsvrtfprdw08 lxpgsvrtfprdw07 lxpgsvrtfprdw06 lxpgsvrtfprdw05 lxpgsvrtfprdw04 lxpgsvrtfprdw03 lxpgsvrtfprdw02 lxpgsvrtfprdw01 lxpgsorbtprdw04 lxpgsorbtprdw02 lxpgsorbtprdw01 lxpgsorbtprdw03 lxpgsgdfrprdw01 lxpgsphneprdw05 lxpgsphneprdw04 lxpgsphneprdw02 lxpgscdpiprdw02 lxpgscdpiprdw01 lxpgsprmeprdw01 lxapppgseprd001 lxpgsdbasprdw01 lxpgswfmsprdw03 lxpgswfmsprdw04 lxpgsspmpprdw01 lxpgsspmpprdw02 lxpgsmrchprdw02 lxpgsmrchprdw01 lxpgsllmsprdw01 lxpgssnrrprdw02 lxpgssnrrprdw01 lxpgspsolprdw02 lxpgspsolprdw01 lxpgssodeprdw02 lxpgssodeprdw01 lxpgslecpprdw02 lxpgslecpprdw01 lxpgsezssprdw02 lxpgsezssprdw01 lxpgslamaprdw02 lxpgslamaprdw01 lxpgskiskprdw01 lxpgskiskprdw03 lxpgskiskprdw02 lxpgsdruprdw004 lxpgsdruprdw003 lxpgsdruprdw002 lxpgsdruprdw001 lxpgskcdeprdw03 lxpgskcdeprdw02 lxpgskcdeprdw01 lxpgshawkprdw05 lxpgshawkprdw02 lxpgshawkprdw01 lxpgskdmzprdw03 lxpgskdmzprdw02 lxpgskdmzprdw01 lxpgsscpdprdw02 lxpgscstlprdw01 lxpgsenasprdw01 lxpgsenasprdw02 lxpgsthorprdw04 lxpgsthorprdw03 lxpgcrpdhprdw08 lxpgcrpdhprdw07 lxpgsdmlpprdw03 lxpgsdmlpprdw02 lxpgsgrafprdw04 lxpgsgrafprdw03 lxpgsdaciprdw01 lxpgsdaciprdw02 lxpgsscprprdw01 lxpgsscprprdw02 lxpgsebinprdw01 lxpgsdaitprdw04 lxpgsdaitprdw01 lxpgsclsdprdw02 lxpgsclsdprdw01 lxpgsstorprdw02 lxpgsstorprdw01 lxpgsetfsprdw04 lxpgsetfsprdw03 lxpgsetfsprdw02 lxpgsetfsprdw01 lxpgssimsprdw16 lxpgssimsprdw15 lxpgssimsprdw14 lxpgssimsprdw13 lxpgssimsprdw12 lxpgssimsprdw11 lxpgssimsprdw10 lxpgssimsprdw09 lxpgssimsprdw08 lxpgssimsprdw07 lxpgssimsprdw06 lxpgssimsprdw05 lxpgssimsprdw04 lxpgssimsprdw03 lxpgssimsprdw02 lxpgssimsprdw01 lxpgsefdpprdw04 lxpgsefdpprdw03 lxpgsefdpprdw02 lxpgsefdpprdw01 lxpgsllmsprdw02 lxpgsdrudprdw06 lxpgsdrudprdw05 lxpgsdrudprdw04 lxpgspstcprdw01 lxpgspstcprdw02 lxpgsediiprdw06 lxpgsediiprdw05 lxpgsediiprdw04 lxpgsediiprdw03 lxpgsediiprdw02 lxpgsediiprdw01 lxpgsemdmprdw02 lxpgsemdmprdw01 lxpgssrpsprdw01 lxpgssrpsprdw02 lxpgsssrmprdw05 lxpgsssrmprdw03 lxpgsssrmprdw01 lxpgsssrmprdw04 lxpgshdatprdw04 lxpgshdatprdw03 lxpgshdatprdw02 lxpgshdatprdw01 lxpgsreprprdw02 lxpgsreprprdw01 lxpgsbrckprdw03 lxpgsbrckprdw02 lxpgsbrckprdw01 lxpgsldspprdw02 lxpgsldspprdw01 lxpgscorpprdw07 lxpgscorpprdw06 lxpgscorpprdw05 lxpgscorpprdw04 lxpgscorpprdw03 lxpgscorpprdw02 lxpgsmaasprdw02 lxpgsmaasprdw01 lxpgssedlprdw02 lxpgssedlprdw01 lxpgstomgprdw02 lxpgstomgprdw01 lxpgsopreprdw01 lxpgsopreprdw02 lxpgsspmtprdw03 lxpgsspmtprdw01 lxpgsspmtprdw04 lxpgsspmtprdw02 lxpgscpmgprdw02 lxpgscpmgprdw01 lxpgsmrvlprdw03 lxpgsmrvlprdw04 lxpgsasnsprdw02 lxpgsasnsprdw01 lxpgsreplprdw01 lxpgsreplprdw02 lxpgslirpprdw02 lxpgslirpprdw01 lxpgssimsprdw23 lxpgssimsprdw21 lxpgssimsprdw19 lxpgssimsprdw17 lxpgssimsprdw24 lxpgssimsprdw22 lxpgssimsprdw20 lxpgssimsprdw18 lxpgsccapprdw02 lxpgsccapprdw01 lxpgslumsprdw02 lxpgslumsprdw01 lxpgslmgrprdw02 lxpgslmgrprdw01 lxpgslpseprdw01 lxpgslpseprdw02 lxpgsansbprdw03 lxpgsansbprdw01 lxpgsansbprdw02 lxpgsmsbrprdw02 lxpgsmsbrprdw01 lxpgsbfstprdw01 lxpgstestprdw02 lxpgstestprdw01 lxpgsetcdprdw05 lxpgsetcdprdw04 lxpgsetcdprdw03 lxpgssossprdw01 lxpgsetcdprdw10 lxpgsdashprdw02 lxpgsdashprdw01 lxpgscarbprdw01)
+
+#Add all the hosts already checked into this list.
+checked_hosts=()
+#Add all the hosts in the current cluster. Will be cleared at the end of each iteration.
+current_cluster_nodes=()
+
+for host in ${hosts[@]};
+do
+    if [[ " ${checked_hosts[*]} " == *" $host "*  ]];
+    then
+        continue
+    fi
+
+    IP=`ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no $host 'hostname -i'`
+
+    #Some servers can't be reached through ssh.
+    if [[ -z $IP ]];
+    then
+        echo "Unable to ssh to $host"
+        continue
+    fi
+
+    #Get the scope of the Patroni cluster through Patroni REST API
+    cluster_name=`wget --timeout 5 -O - -q -t 1 http://$IP:8008/patroni|jq -r '.patroni'|jq -r '.scope'`
+    #Get all the nodes of a Patroni cluster through Patroni REST API
+    cluster_nodes=`wget --timeout 5 -O - -q -t 1 http://$IP:8008/cluster | jq|grep -i name|awk '{print $2}'|sed 's/[",]//g' | xargs`
+    
+    #Add the nodes to the lists initialized above.
+    if [[ -n $cluster_nodes ]];
+    then
+        for node in $cluster_nodes;
+        do
+        checked_hosts+=("$node")
+        current_cluster_nodes+=("$node")
+        done
+    fi
+
+
+    if [[ -n $cluster_nodes ]];
+    then
+        echo "======================================================================================================================================================"
+        for node in ${current_cluster_nodes[@]};
+        do
+            echo "$node :"
+            cpu_count=$(ssh "$node" 'lscpu | grep -E "^CPU\(s\):[[:space:]]+[0-9]+" | awk "{print \$2}"')
+            ram_total=$(ssh "$node" 'lsmem | grep -i "Total online memory" | awk "{print \$4}"')
+            echo "CPU : $cpu_count"
+            echo "RAM : $ram_total"
+        done
+        echo "======================================================================================================================================================"
+
+    elif [[ -z $cluster_nodes ]];
+    then
+        echo "======================================================================================================================================================"
+        printf "\n\n"
+        echo "$host is a standalone node."
+        printf "\n\n"
+        echo "======================================================================================================================================================"
+
+    fi
+
+    
+    current_cluster_nodes=()
+done 
+
